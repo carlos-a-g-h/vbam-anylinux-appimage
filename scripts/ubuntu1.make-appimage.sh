b4438678899a65066cdc540c6241e8053f1d80ff
carlos-a-g-h <carloselputohabanero@proton.me>
2025-12-14 06:56:22 -0500

    Add files via upload

--- scripts/ubuntu1.make-appimage.sh
+++ scripts/ubuntu1.make-appimage.sh
@@ -10,50 +10,48 @@ ARCH=$(uname -m)
 VERSION="v2.1.0"
 NAME="VisualBoyAdvance-M"
 
-PATH_SOURCECODE="$(realpath -e visualboyadvance-m.source)"
 PATH_DESKTOP=$(realpath -e visualboyadvance-m.desktop)
 
 # APPIMAGE_STEM="$NAME"_"$VERSION"_"$GH_SHA_SHORT"_"$ARCH"
 APPIMAGE_STEM="$NAME"_"$VERSION"_"$GH_SHA_SHORT"_anylinux_"$ARCH"
 
+
 export ARCH VERSION
 # export ADD_HOOKS="self-updater.bg.hook"
 # export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=$(realpath -e "$PATH_SOURCECODE"/src/wx/icons/sizes/256x256/apps/visualboyadvance-m.png)
+export ICON="/usr/share/icons/visualboyadvance-m.png"
 export DESKTOP=$(realpath -e visualboyadvance-m.desktop)
 export OUTNAME="$APPIMAGE_STEM".AppImage
 export OUTPATH=./dist
 
-export NO_STRIP=1
+#export NO_STRIP=1
 export DEPLOY_OPENGL=1
 export DEPLOY_GEGL=0
-export DEPLOY_PULSE=1
-export DEPLOY_PIPEWIRE=1
+export DEPLOY_PULSE=0
+export DEPLOY_PIPEWIRE=0
 export DEPLOY_VULKAN=0
-export DEPLOY_GTK=1
+export DEPLOY_GTK=0
 export DEPLOY_SDL=1
 export DEPLOY_GLYCIN=0
 
-# Copy the full github SHA
+cp -v "/usr/share/icons/hicolor/256x256/apps/vbam.png" "$ICON"
+
+# Deploy dependencies
+./quick-sharun.sh \
+	/usr/games/vbam /usr/games/visualboyadvance-m
+
+# Copy data files
+python3 hhs/script_deploy.py \
+	AppDir/shared \
+	vbam vbam-common vbam-gtk vbam-sdl
+
+rm -rf AppDir/shared/usr/games
 
 mkdir -vp AppDir/details
 echo "$GH_SHA" > AppDir/details/commit.txt
+cp -va ubuntu1/* AppDir/details/
 
 mkdir -p "$OUTPATH"
-find AppDir > "$OUTPATH"/"$APPIMAGE_STEM".txt
-
-#mkdir -vp AppDir/usr/bin
-#mkdir -vp AppDir/shared/bin
-#cp -va "$PATH_ALL_ICONS"/* AppDir/usr/share/icons/scalable/
-
-# Copy the missing libwx files, including localization files
-# mkdir -vp AppDir/share/aclocal
-# cp -v /usr/share/aclocal/wxwin.m4 AppDir/share/aclocal/
-
-# Deploy dependencies
-./quick-sharun.sh ./vbam ./visualboyadvance-m
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 ./quick-sharun.sh --make-appimage
