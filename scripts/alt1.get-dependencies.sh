086ff9ebc1f801ed5a5fb0a3007c2ed8b7f2374a
carlos-a-g-h <carloselputohabanero@proton.me>
2025-12-18 21:35:27 -0500

    Add files via upload

--- scripts/alt1.get-dependencies.sh
+++ scripts/alt1.get-dependencies.sh
@@ -2,10 +2,7 @@
 
 set -eux
 
-# VBA-M 2.0.1 r201807121140-0e60c34
-
-# NOTE:
-# The vbam packages are versioned as 2.0.1 but the binaries inside are 2.1.0. Possible typo...?
+# There's no apt,on ALT, there is apt-get
 
 ################################################################################
 echo "DOWNLOADING EVERYHTING"
