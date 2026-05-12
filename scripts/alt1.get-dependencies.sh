b390b79ade829867563beec6c500d5c4dadaa5d0
carlos-a-g-h <carloselputohabanero@proton.me>
2026-05-12 11:43:03 -0400

    squashfs-tools

--- scripts/alt1.get-dependencies.sh
+++ scripts/alt1.get-dependencies.sh
@@ -9,7 +9,7 @@ echo "DOWNLOADING EVERYHTING"
 
 # apt-get update
 
-apt-get install -yy git wget zsync patchelf xorg-xvfb binutils build-essential fastfetch
+apt-get install -yy git wget zsync patchelf xorg-xvfb binutils build-essential fastfetch build-essential squashfs-tools
 
 URL_SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
