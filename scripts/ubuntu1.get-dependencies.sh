ad424e02cdb52fac9dc8d1a08e03ce6a1cb1cc7c
carlos-a-g-h <carloselputohabanero@proton.me>
2026-05-12 11:43:32 -0400

    Update ubuntu1.get-dependencies.sh

--- scripts/ubuntu1.get-dependencies.sh
+++ scripts/ubuntu1.get-dependencies.sh
@@ -12,7 +12,7 @@ echo "DOWNLOADING EVERYHTING"
 
 apt update
 
-apt install -yy git wget zsync patchelf xvfb binutils build-essential
+apt install -yy git wget zsync patchelf xvfb binutils build-essential squashfs-tools
 
 # URL_HELPERS="https://github.com/carlos-a-g-h/dependency-helper-scripts"
 URL_SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
